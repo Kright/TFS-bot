5bcdc42d04e0ce4dacdda5100ad68e5cc4674a47
Igor Slobodskov <igor.slobodskov@phystech.edu>
2017-05-09 12:34:05 +0300

    add logging

--- src/main/scala/bot/telegram/Logging.scala
+++ src/main/scala/bot/telegram/Logging.scala
@@ -1,23 +1,34 @@
 package bot.telegram
 
 import bot.telegram.api._
+import com.typesafe.scalalogging.{Logger, StrictLogging}
+import org.slf4j.LoggerFactory
+
+import scala.util.{Failure, Success, Try}
 
 /**
   * Created by lgor on 4/16/17.
   */
-trait Logging extends TelegramBot {
+trait TelegramUpdatesLogging extends TelegramBot with StrictLogging {
 
-  abstract override def apply(sendData: SendData) = {
-    println(s"sending : $sendData")
+  abstract override def requestUpdates(timeoutInSeconds: Int): List[Update] =
+    Try {
+      super.requestUpdates(timeoutInSeconds)
+    } match {
+      case Success(updates) => logger.debug(s"updates = {${updates.mkString(",\n")}}"); updates
+      case Failure(exception) => logger.error(s"requestUpdates() error: $exception"); throw exception
+    }
+}
+
+trait TelegramSendLogging extends TelegramBot with StrictLogging {
+
+  abstract override def apply(sendData: SendData): SendResult = {
     val result = super.apply(sendData)
-    log(result)
-
+    result match {
+      case SendSuccess => logger.debug(s"sending: $sendData")
+      case SendFailed(response) => logger.error(s"send failed: $response")
+      case SendError(ex) => logger.error(s"send error: $ex")
+    }
     result
-  }
-
-  private def log(sr: SendResult): Unit = sr match {
-    case SendFailed(response) => println(s"sending failed: $response")
-    case SendError(ex) => println(s"sending exception: $ex")
-    case SendSuccess =>
   }
 }
